c9a928a23ea60309004500208ad21fef27433030
Karthick2001-dev <karthickthirumalai06@gmail.com>
2025-01-27 12:37:48 +0530

    Upated the size issues

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
+    const menuLinks = document.querySelector('.menu-links');
+    menuLinks.classList.toggle('active')
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
